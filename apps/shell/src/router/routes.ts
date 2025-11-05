63cbad53eb1056132edb708cca5964c6c02d1f20
DmitrySinyapkin <dsinyapkin@mail.ru>
2025-11-05 13:13:29 +0300

    [shell] - fixed dashboard routes

--- apps/shell/src/router/routes.ts
+++ apps/shell/src/router/routes.ts
@@ -13,7 +13,7 @@ const routes: RouteRecordRaw[] = [
         }
       },
       {
-        path: 'dashboard',
+        path: 'dashboard:catchAll(.*)*',
         component: () => import('components/remoteWrapper/RemoteWrapper.vue'),
         props: {
           remoteApp: 'dashboard'
